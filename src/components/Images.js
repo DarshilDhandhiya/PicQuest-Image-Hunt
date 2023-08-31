0f0b13dd7a67dfb8929226356fb680f24e6c3b1e
DarshilDhandhiya <darshil.dhandhiya03@gmail.com>
2023-08-31 20:32:11 +0530

    API

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,7 +1,19 @@
 const Images = () => {
   return (
     <>
-        <h1>React For Cats</h1>
+        <h1 className="text-center mt-6 underline text-2xl">React For Cats</h1>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10">
+            <h1>Image 1</h1>
+            <h1>Image 2</h1>
+            <h1>Image 3</h1>
+            <h1>Image 4</h1>
+            <h1>Image 5</h1>
+            <h1>Image 6</h1>
+            <h1>Image 7</h1>
+            <h1>Image 8</h1>
+            <h1>Image 9</h1>
+            <h1>Image 10</h1>
+        </div>
     </>
   )
 }
