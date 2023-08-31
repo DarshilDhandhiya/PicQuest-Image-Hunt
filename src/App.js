97eb115ebecc795247325e9e41c13530a3b40eed
DarshilDhandhiya <darshil.dhandhiya03@gmail.com>
2023-08-31 20:52:53 +0530

    Hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import useAxios from './hooks/useAxios.JS';
 import Images from './components/Images'; // Adjust the path according to your project structure
 import Jumbutron from "./components/Jumbutron";
 import SearchField from "./components/SearchField";
 
 
 function App() {
+
+  const{ response, isLoading, error, fetchData } = useAxios()
+
   return (
     <>
       <Jumbutron>
