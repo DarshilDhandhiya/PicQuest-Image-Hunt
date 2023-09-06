1467aa2af90d2d4d37ea82205c90f649ba0bb5d1
DarshilDhandhiya <darshil.dhandhiya03@gmail.com>
2023-09-07 00:26:41 +0530

    Delete Main Project

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# PicQuest-Image-Hunt
-"SnapJoy: Hunt Awesome Images with React! Your go-to Unsplash-powered app for discovering captivating photos. Unleash the pixels!"
--- src/components/Skeleton.js
+++
@@ -1,9 +0,0 @@
-const Skeleton = ({ item }) => {
-    return [...Array(item).keys()].map(() => (
-      <div className="animate-pulse">
-        <div className="bg-gray-300 rounded-lg h-72"></div>
-      </div>
-    ))
-  }
-  
-  export default Skeleton
